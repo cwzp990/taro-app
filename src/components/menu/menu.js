a2d39828a5f7264617e691b3d84b9afe9215f10c
cwzp990 <cwzp990@gmail.com>
2019-04-21 11:19:43 +0800

    抽屉组件

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Image, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
+import { AtDrawer } from 'taro-ui'
 import { setCurrentCat, setDrawerStatus } from '../../actions/menu'
 
 import './menu.scss'
@@ -10,16 +11,38 @@ class Menu extends Component {
     super(props)
   }
 
+  componentWillMount () {
+    this.menuItem = ['全部', '精华', '分享', '问答', '招聘', '客户端测试']
+  }
+
   showDrawer = () => {
     this.props.setDrawerStatus(true)
   }
 
+  onClose = () => {
+    this.props.setDrawerStatus(false)
+  }
+
+  onItem = (index) => {
+    this.props.setCurrentCat(this.menuItem[index])
+  }
+
   render () {
+    const { status } = this.props
     return (
-      <View className='m-Menu'>
-        <Image onClick={this.showDrawer} className='m-Menu-icon' src={require('../../assets/menu.png')} />
-        <Text>{this.props.current}</Text>
-        <Image className='m-Menu-icon' src={require('../../assets/user.png')} />
+      <View>
+        <View className='m-Menu'>
+          <Image onClick={this.showDrawer} className='m-Menu-icon' src={require('../../assets/menu.png')} />
+          <Text>{this.props.current}</Text>
+          <Image className='m-Menu-icon' src={require('../../assets/user.png')} />
+        </View>
+        <AtDrawer
+          show={status}
+          mask
+          onClose={this.onClose.bind(this)}
+          onItemClick={this.onItem.bind(this)}
+          items={this.menuItem}
+        ></AtDrawer>
       </View>
     )
   }
@@ -27,10 +50,11 @@ class Menu extends Component {
 
 // 映射Redux全局的state到组件的props上 (接收)
 const mapStateToProps = state => ({
-  current: state.current,
-  status: state.status
+  current: state.currentCat,
+  status: state.drawerStatus
 })
 
+// 映射dispatch到props (发送)
 const mapDispatchToProps = dispatch => ({
   setCurrentCat: status => {
     dispatch(setCurrentCat(status))
--- src/reducers/menu.js
+++ src/reducers/menu.js
@@ -1,15 +1,7 @@
 import * as types from '../constants/counter'
 
 const MENU_STATE = {
-  category: [
-    { key: 'all', value: '全部' },
-    { key: 'good', value: '精华' },
-    { key: 'share', value: '分享' },
-    { key: 'ask', value: ' 问答' },
-    { key: 'job', value: ' 招聘' },
-    { key: 'dev', value: ' 客户端测试' },
-  ],
-  current: 'all',
+  current: '全部',
   status: false
 }
 
